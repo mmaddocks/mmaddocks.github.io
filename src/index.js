85b04c60db1a78fb8384d0da6fd673079a8b10b6
Mark Maddocks <mark.emaddocks@gmail.com>
2021-01-02 19:22:35 +0000

    Added 404 and script to index to handle client side routing

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import { BrowserRouter as Router } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  // <Router>
-  //   <App/>
-  // </Router>,
-  <HashRouter>
+  <Router>
     <App/>
-  </HashRouter>,
+  </Router>,
+  // <HashRouter>
+  //   <App/>
+  // </HashRouter>,
   document.getElementById('root')
 );
 
