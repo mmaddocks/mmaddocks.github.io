1d6a6be392d4baebacbec96e3a8f81b9305f986c
Mark Maddocks <mark.emaddocks@gmail.com>
2021-02-20 17:47:31 +0000

    Google analytics changed

--- src/index.js
+++ src/index.js
@@ -5,8 +5,24 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import { HashRouter } from 'react-router-dom';
 
+// GA
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+
+// Google Analytics
+const TRACKING_ID = 'UA-83611733-2';
+ReactGA.initialize(TRACKING_ID);
+
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <App/>
   </Router>,
   // <HashRouter>
